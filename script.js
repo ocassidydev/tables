26b63d80ac9b75e91cb2c567532f39f47d0ff42f
ocassidydev <ocassidy@tcd.ie>
2022-12-15 10:53:43 +0000

    Completed tutorial

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+}); 
+
+$(document).ready(function() {
+	$("h2").addClass("underline");
+	$("ul").addClass("border" );
+});
